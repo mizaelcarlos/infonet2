8dc12eb67317534547524e16e50653807ab39f55
Mizael Carlos <mizaelcarlos44@gmail.com>
2024-12-03 10:31:25 -0300

    Posts em colunas

--- Codificação para BackEnd/Criar Sistema usando Laravel/blog/resources/views/post/index.blade.php
+++ Codificação para BackEnd/Criar Sistema usando Laravel/blog/resources/views/post/index.blade.php
@@ -2,26 +2,27 @@
     @extends('layouts.app')
 
     @section('content') 
-            @foreach($posts as $post)
-            
-            
+            <div class="container">
+                <div class="row">
+                @foreach($posts as $post)
+                <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ asset('storage/'.$post->foto)}}" alt="Imagem de capa do card">
+                        <div class="card-body">
+                            <h5 class="card-title">{{$post->titulo}}</h5>
+                            <p class="card-text">{{$post->conteudo}}</p>
+                            <form action="{{route('post.destroy', $post->id)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Excluir</button>
+                            </form> 
+                            <button type="button" class="btn btn-success"><a href="{{ route('post.edit', $post->id) }}">Editar</a></button>
+                            <button type="button" class="btn btn-success"><a href="{{ route('post.show', $post->id) }}">Visualizar</a></button>
 
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top" src="{{ asset('storage/'.$post->foto)}}" alt="Imagem de capa do card">
-                <div class="card-body">
-                    <h5 class="card-title">{{$post->titulo}}</h5>
-                    <p class="card-text">{{$post->conteudo}}</p>
-                    <form action="{{route('post.destroy', $post->id)}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit">Excluir</button>
-                    </form> 
-                    <button><a href="{{ route('post.edit', $post->id) }}">Editar</a></button>
-                    <button><a href="{{ route('post.show', $post->id) }}">Visualizar</a></button>
-
-                </div>
+                        </div>
+                    </div>
+                |</div>
+                @endforeach
             </div>
-            @endforeach
-       
- 
+        </div>
     @endsection
